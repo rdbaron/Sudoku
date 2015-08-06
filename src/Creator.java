dc1723da864c3cd047090ffb5d6fe88c62b98719
Bob Baron <rdbaron@gmail.com>
2015-08-05 20:19:28 -0400

    Creator now working.  Generates puzzle of random difficulty instead of expressed number of values.

--- src/Creator.java
+++ src/Creator.java
@@ -5,10 +5,11 @@ import java.util.Random;
 
 public class Creator {
 
-	public static boolean createFromScratch(int[][] board) {
+	public static int[][] createFromScratch(int[][] board) {
+		//In order to produce a new game board, start by creating a fully filled out board, then remove values until you can't remove more.
 		buildBoard(board, 0, 0);
-		return removeValues(board, 25);
-		//return createFromScratch2(board);
+	
+		return removeValues(board);
 	}
 	
 	public static boolean buildBoard(int[][] board) {
@@ -51,40 +52,9 @@ public class Creator {
 				throw new Exception ("Too many rows!");
 			else
 				return board;
-		}
-		
-		
+		}			
 	}
-	
-	/*public static boolean buildBoard(int[][] board) {
-		Random rn = new Random();
 		
-		if (Solver.validateUnique(board) == 1) 
-			return true;
-		else {
-			int row = rn.nextInt(10);
-			int col = rn.nextInt(10);
-			
-			if (board[row][col] == 0) {
-				//If you can fill board[row][col], attempt to fill board[col][row] for symmetry
-				if (fillCell(board, row, col)) {
-					if (!fillCell(board, col, row))
-						//Reset board[row][col] if board[col][row] fails
-						board[row][col] = 0;
-				}
-				
-			}
-			
-			if (buildBoard(board))
-				return true;
-			
-			board[row][col] = 0;
-			board[col][row] = 0;
-			
-		}
-		return false;
-	}*/
-	
 	private static boolean fillCell(int[][] board, int row, int col) {
 		HashSet<Integer> tried = new HashSet<Integer>();
 		
@@ -117,104 +87,50 @@ public class Creator {
 				}
 				
 				board[row][col] = 0;
-			}
-			
+			}	
 		}
 		return false;
 	}
-	
-	private static boolean removeValues(int[][] board, int numRemoves) {
-		//Create an index of cells
-		HashSet<Integer> cells= new HashSet<Integer>();
-		
-		for (int i = 0; i < 81; i++)
-			cells.add(i); 
-		
-		return removeValues(board, numRemoves, cells);
-		
-	}
 
-	private static boolean removeValues(int[][] board, int numRemoves, HashSet<Integer> cells) {
-		System.out.println("Number of removes left: " + numRemoves);
-		if (numRemoves == 0)
-			return true;
-		
+	private static int[][] removeValues(int[][] board) {
 		int[][] testBoard;
 		Random rn = new Random();
 		
-		while (cells.size() > 0) {
-						
+		//Create an index of cells
+		HashSet<Integer> cells= new HashSet<Integer>();
+				
+		for (int i = 0; i < 81; i++)
+			cells.add(i); 
+		
+		//Loop through the cells and randomly try removing a matching pair (or the middle cell by itself
+		//Increment by two because two cells will be removed each time
+		for (int i = 0; i < 81; i += 2) {
 			int cell = rn.nextInt(cells.size());
 			
 			int row = (int) Math.floor(cell/9);
 			int col = cell % 9;
+			//Store existing values in case we need to revert
+			int[] curValues = {board[row][col], board[8-row][8-col]};
 			
-			//Make sure the cell you just grabbed is not already 0, continue until you find one that isn't.
-			while (board[row][col] == 0) {
-				System.out.println("Cell was already 0");
-				cell = rn.nextInt(cells.size());
-				
-				row = (int) Math.floor(cell/9);
-				col = cell % 9;
-			}
-			
-			int curValue = board[row][col];
 			board[row][col] = 0;
+			board[8-row][8-col] = 0;
 			testBoard = board;
-			cells.remove(cell);
 			
-			
-			System.out.println("Removed cell " + cell);
-			if (Solver.validateUnique(testBoard) == 1) {
-				System.out.println("Puzzle is still valid.");
-				if (removeValues(board, numRemoves-1, cells))
-					return true;
-				//else
-					//numRemoves += 1;
+			if (Solver.validateUnique(testBoard) != 1) {
+				//An invalid puzzle was produced, reset these values
+				board[row][col] = curValues[0];
+				board[8-row][8-col] = curValues[1];
 			}
+
+			//Either way, remove these cells from the list of possibilities
+			cells.remove(cell);
+			cells.remove(80-cell);
 			
-			board[row][col] = curValue;
-			numRemoves += 1;
-			System.out.println("Puzzle no longer valid.  numRemoves set back to " + numRemoves);
-			return false;
 		}
 		
-		return false;
-		
+		return board;		
 	}
 	
-	/*private static boolean createFromScratch2(int[][] board) {
-		System.out.println("Number of soln: " + Solver.validateUnique(board));
-		if (Solver.validateUnique(board) == 1) 
-			return true;
-		
-		//Randomly select a cell to fill
-		Random rn = new Random();
-		int row = rn.nextInt(9);
-		int col = rn.nextInt(9);
-		
-		System.out.println("Row and Col: " + row + " " + col);
-		System.out.println("Current value: " + board[row][col]);
-		//If you hit an already filled cell, try again
-		if (board[row][col] != 0)
-			return createFromScratch2(board);
-		
-		//Fill the cell, and ensure a valid solution remains
-		if (fillCell(board, row, col)) {
-			System.out.println("New value: " + board[row][col]);
-			System.out.println("Number of soln changes to: " + Solver.validateUnique(board));
-			if (Solver.validateUnique(board) > 0) {
-				
-				if (createFromScratch2(board)) {
-					return true;
-				}
-			}
-		}
-		
-		System.out.println("Switch value back to 0");
-		board[row][col] = 0;
-		return false;
-	}*/
 }
 
 
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -2,11 +2,6 @@ public class Sudoku {
 
 		public static void main(String[] args) {
 			
-			/*int[][] board = {{5,1,0,0,0,0,0,8,3},{8,0,0,4,1,6,0,0,5},{0,0,0,0,0,0,0,0,0},
-								{0,9,8,5,0,4,6,1,0},{0,0,0,9,0,1,0,0,0},{0,6,4,2,0,3,5,7,0},
-								{0,0,0,0,0,0,0,0,0},{6,0,0,1,5,7,0,0,4},{7,8,0,0,0,0,0,9,6}};
-			*/
-			
 			try {
 				//int[][] board = Creator.createFromFile("C:\\sudoku\\easy2.txt"); 
 				int[][] board = new int[9][9];
